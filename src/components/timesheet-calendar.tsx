18ffbe4f5dfc39fbfa99bf3be437cd3040d4d60d
Roberto Coratti <roberto.coratti@assertcode.it>
2025-06-16 15:02:13 +0200

    changes

--- src/components/timesheet-calendar.tsx
+++ src/components/timesheet-calendar.tsx
@@ -36,40 +36,123 @@ import { EntryWithClient } from "@/db/schema"
 import { cn } from "@/lib/utils"
 import { useDialog } from "@/hooks/use-dialog"
 import { DialogId } from "@/contexts/dialog-context"
+import { useIsMobile } from "@/hooks/use-mobile"
+import { ToggleGroup, ToggleGroupItem } from "./ui/toggle-group"
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from "./ui/select"
 
-const WEEK_DAYS = eachDayOfInterval({
-	start: startOfWeek(new Date(), { weekStartsOn: 1 }),
-	end: addDays(startOfWeek(new Date(), { weekStartsOn: 1 }), 6),
-}).map((date) => format(date, "EEEE"))
+type TimesheetCalendarViewType = "month" | "week" | "day"
 
 interface TimesheetCalendarHeaderProps {
 	currentDate: Date
 	onDateChange: (date: Date) => void
+	viewType: TimesheetCalendarViewType
+	onViewTypeChange: (viewType: TimesheetCalendarViewType) => void
 }
 
 export function TimesheetCalendarHeader({
 	currentDate,
 	onDateChange,
+	viewType,
+	onViewTypeChange,
 }: TimesheetCalendarHeaderProps) {
 	return (
-		<div className="w-full flex items-center justify-center gap-4">
+		<div className="w-full grid grid-rows-2 grid-cols-2 md:grid-rows-1 md:grid-cols-3 px-2 md:place-items-center">
 			<Button
 				variant="outline"
-				size="icon"
-				onClick={() => onDateChange(subMonths(currentDate, 1))}
+				className="place-self-start"
+				onClick={() => onDateChange(new Date())}
 			>
-				<ChevronLeft className="h-5 w-5" />
+				Oggi
 			</Button>
-			<div suppressHydrationWarning className="text-lg font-semibold">
-				{format(currentDate, "MMMM yyyy")}
+			<div className="flex items-center gap-4">
+				<Button
+					variant="outline"
+					size="icon"
+					onClick={() => onDateChange(subMonths(currentDate, 1))}
+				>
+					<ChevronLeft className="h-5 w-5" />
+				</Button>
+				<div suppressHydrationWarning className="text-lg font-semibold">
+					{format(currentDate, "MMMM yyyy")}
+				</div>
+				<Button
+					variant="outline"
+					size="icon"
+					onClick={() => onDateChange(addMonths(currentDate, 1))}
+				>
+					<ChevronRight className="h-5 w-5" />
+				</Button>
 			</div>
-			<Button
-				variant="outline"
-				size="icon"
-				onClick={() => onDateChange(addMonths(currentDate, 1))}
-			>
-				<ChevronRight className="h-5 w-5" />
-			</Button>
+			<React.Fragment>
+				<ToggleGroup
+					type="single"
+					variant="outline"
+					value={viewType}
+					onValueChange={(val) => {
+						if (val) onViewTypeChange(val as TimesheetCalendarViewType)
+					}}
+					className="hidden place-self-end *:data-[slot=toggle-group-item]:!px-4 md:flex"
+				>
+					<ToggleGroupItem
+						value={"month" as TimesheetCalendarViewType}
+						aria-label="Toggle month view type"
+					>
+						Mese
+					</ToggleGroupItem>
+					<ToggleGroupItem
+						value={"week" as TimesheetCalendarViewType}
+						aria-label="Toggle week view type"
+					>
+						Settimana
+					</ToggleGroupItem>
+					<ToggleGroupItem
+						value={"day" as TimesheetCalendarViewType}
+						aria-label="Toggle day view type"
+					>
+						Giorno
+					</ToggleGroupItem>
+				</ToggleGroup>
+				<Select
+					value={viewType}
+					onValueChange={(selectedViewType) => {
+						onViewTypeChange(selectedViewType as TimesheetCalendarViewType)
+					}}
+				>
+					<SelectTrigger
+						className="flex w-40 **:data-[slot=select-value]:block **:data-[slot=select-value]:truncate md:hidden"
+						size="sm"
+						aria-label="Select a value"
+					>
+						<SelectValue placeholder="Ultimi 3 mesi" />
+					</SelectTrigger>
+					<SelectContent className="rounded-xl">
+						<SelectItem
+							value={"month" as TimesheetCalendarViewType}
+							className="rounded-lg"
+						>
+							Mese
+						</SelectItem>
+						<SelectItem
+							value={"week" as TimesheetCalendarViewType}
+							className="rounded-lg"
+						>
+							Settimana
+						</SelectItem>
+						<SelectItem
+							value={"day" as TimesheetCalendarViewType}
+							className="rounded-lg"
+						>
+							Giorno
+						</SelectItem>
+					</SelectContent>
+				</Select>
+			</React.Fragment>
 		</div>
 	)
 }
@@ -132,13 +215,21 @@ function TimesheetCalendarEntryPill({
 }
 
 export function TimesheetCalendar() {
+	const isMobile = useIsMobile()
 	const [date, setDate] = React.useState(new Date())
+	const [viewType, setViewType] = React.useState<TimesheetCalendarViewType>(
+		isMobile ? "week" : "month"
+	)
 	const days = getCalendarMatrix(date)
 	const [selectedEntry, setSelectedEntry] =
 		React.useState<EntryWithClient | null>(null)
 	const [selectedDate, setSelectedDate] = React.useState<Date>()
 	const { entries } = useEntries()
 	const { openDialog, closeDialog } = useDialog()
+
+	React.useEffect(() => {
+		setViewType(isMobile ? "week" : "month")
+	}, [isMobile])
 
 	function getEditDialogId(entry: EntryWithClient): DialogId {
 		switch (entry.type) {
@@ -155,153 +246,181 @@ export function TimesheetCalendar() {
 		}
 	}
 
-	return (
-		<React.Fragment>
-			<div className="flex-1 h-full flex flex-col border-2 border-border rounded-t-2xl overflow-hidden">
-				<div className="py-2">
-					<TimesheetCalendarHeader currentDate={date} onDateChange={setDate} />
-				</div>
-				<div className="grid grid-cols-7">
-					{WEEK_DAYS.map((d) => (
-						<div
-							key={d}
-							className="bg-card border-1 py-2 text-sm font-medium text-center text-card-foreground"
-							suppressHydrationWarning
-						>
-							{d}
-						</div>
-					))}
-				</div>
-				<div className="flex-1 grid grid-cols-7">
-					{days.map((day) => {
-						const dayEntries = entries.filter((e) =>
-							isSameDay(new Date(e.date), day)
-						)
+	const WEEK_DAYS = React.useMemo(
+		() =>
+			eachDayOfInterval({
+				start: startOfWeek(new Date(), { weekStartsOn: 1 }),
+				end: addDays(startOfWeek(new Date(), { weekStartsOn: 1 }), 6),
+			}).map((date) => format(date, isMobile ? "E" : "EEEE")),
+		[isMobile]
+	)
 
-						return (
-							<ContextMenu key={day.toISOString()}>
-								<ContextMenuTrigger
-									asChild
-									onContextMenu={(e) => {
-										const isEntry = (e.target as HTMLElement).closest(
-											"[data-entry]"
-										)
-										if (!isEntry) {
-											setSelectedDate(day)
-											setSelectedEntry(null)
-										}
-									}}
-								>
-									<div
-										className={cn(
-											"transition-all flex flex-col border-1 gap-1 p-2 bg-card text-card-foreground",
-											!isSameMonth(day, date) && "opacity-40"
-										)}
+	return (
+		<div className="flex-1 h-full flex flex-col md:border-2 border-border md:rounded-t-2xl overflow-hidden">
+			<div className="py-2">
+				<TimesheetCalendarHeader
+					currentDate={date}
+					onDateChange={setDate}
+					viewType={viewType}
+					onViewTypeChange={setViewType}
+				/>
+			</div>
+			{viewType === "month" && (
+				<React.Fragment>
+					<div className="grid grid-cols-7">
+						{WEEK_DAYS.map((d) => (
+							<div
+								key={d}
+								className="bg-card border-1 py-2 text-sm font-medium text-center text-card-foreground"
+								suppressHydrationWarning
+							>
+								{d}
+							</div>
+						))}
+					</div>
+					<div className="flex-1 grid grid-cols-7">
+						{days.map((day) => {
+							const dayEntries = entries.filter((e) =>
+								isSameDay(new Date(e.date), day)
+							)
+
+							return (
+								<ContextMenu key={day.toISOString()}>
+									<ContextMenuTrigger
+										asChild
+										onContextMenu={(e) => {
+											const isEntry = (e.target as HTMLElement).closest(
+												"[data-entry]"
+											)
+											if (!isEntry) {
+												setSelectedDate(day)
+												setSelectedEntry(null)
+											}
+										}}
 									>
-										<span
+										<div
 											className={cn(
-												"text-sm font-medium font-mono max-w-fit ml-auto text-muted-foreground",
-												isToday(day) &&
-													"bg-primary text-primary-foreground px-1 py-0.5 rounded-full"
+												"transition-all flex flex-col border-1 gap-1.5 p-2 bg-card text-card-foreground",
+												!isSameMonth(day, date) && "opacity-25"
 											)}
 										>
-											{day.getDate()}
-										</span>
-										<div className="flex flex-col flex-1 gap-1">
-											{dayEntries.map((entry) => (
-												<div
-													key={entry.id}
-													data-entry
-													onContextMenu={() => {
-														closeDialog()
-														setSelectedEntry(entry)
-														setSelectedDate(undefined)
+											<span
+												className={cn(
+													"text-sm font-medium max-w-fit ml-auto text-muted-foreground",
+													isToday(day) &&
+														"bg-primary text-primary-foreground px-1.5 py-0.5 rounded-full shadow-sm"
+												)}
+											>
+												{day.getDate()}
+											</span>
+											<div className="flex flex-col flex-1 gap-1">
+												{dayEntries.map((entry) => (
+													<div
+														key={entry.id}
+														data-entry
+														onContextMenu={() => {
+															closeDialog()
+															setSelectedEntry(entry)
+															setSelectedDate(undefined)
+														}}
+													>
+														<TimesheetCalendarEntryPill entry={entry} />
+													</div>
+												))}
+											</div>
+										</div>
+									</ContextMenuTrigger>
+									<ContextMenuContent>
+										{selectedEntry ? (
+											<React.Fragment>
+												<ContextMenuItem
+													onClick={() => {
+														if (selectedEntry)
+															openDialog(getEditDialogId(selectedEntry), {
+																entry: selectedEntry,
+															})
 													}}
 												>
-													<TimesheetCalendarEntryPill entry={entry} />
-												</div>
-											))}
-										</div>
-									</div>
-								</ContextMenuTrigger>
-								<ContextMenuContent>
-									{selectedEntry ? (
-										<React.Fragment>
-											<ContextMenuItem
-												onClick={() => {
-													if (selectedEntry)
-														openDialog(getEditDialogId(selectedEntry), {
-															entry: selectedEntry,
-														})
-												}}
-											>
-												<Pencil />
-												Modifica
-											</ContextMenuItem>
-											<ContextMenuItem
-												variant="destructive"
-												onClick={() => {
-													if (selectedEntry)
-														openDialog("confirmDeleteEntry", {
-															entry: selectedEntry,
-														})
-												}}
-											>
-												<Trash />
-												Elimina
-											</ContextMenuItem>
-										</React.Fragment>
-									) : selectedDate ? (
-										<ContextMenuSub>
-											<ContextMenuSubTrigger inset>
-												Aggiungi
-											</ContextMenuSubTrigger>
-											<ContextMenuSubContent className="w-44">
-												<ContextMenuItem
-													onClick={() =>
-														openDialog("createWorkingEntry", {
-															date: selectedDate,
-														})
-													}
-												>
-													Attivitá
+													<Pencil />
+													Modifica
 												</ContextMenuItem>
 												<ContextMenuItem
-													onClick={() =>
-														openDialog("createHolidayEntry", {
-															date: selectedDate,
-														})
-													}
+													variant="destructive"
+													onClick={() => {
+														if (selectedEntry)
+															openDialog("confirmDeleteEntry", {
+																entry: selectedEntry,
+															})
+													}}
 												>
-													Ferie
+													<Trash />
+													Elimina
 												</ContextMenuItem>
-												<ContextMenuItem
-													onClick={() =>
-														openDialog("createPermitEntry", {
-															date: selectedDate,
-														})
-													}
-												>
-													Permesso
-												</ContextMenuItem>
-												<ContextMenuItem
-													onClick={() =>
-														openDialog("createSickEntry", {
-															date: selectedDate,
-														})
-													}
-												>
-													Malattia
-												</ContextMenuItem>
-											</ContextMenuSubContent>
-										</ContextMenuSub>
-									) : null}
-								</ContextMenuContent>
-							</ContextMenu>
-						)
-					})}
+											</React.Fragment>
+										) : selectedDate ? (
+											<ContextMenuSub>
+												<ContextMenuSubTrigger inset>
+													Aggiungi
+												</ContextMenuSubTrigger>
+												<ContextMenuSubContent className="w-44">
+													<ContextMenuItem
+														onClick={() =>
+															openDialog("createWorkingEntry", {
+																date: selectedDate,
+															})
+														}
+													>
+														Attivitá
+													</ContextMenuItem>
+													<ContextMenuItem
+														onClick={() =>
+															openDialog("createHolidayEntry", {
+																date: selectedDate,
+															})
+														}
+													>
+														Ferie
+													</ContextMenuItem>
+													<ContextMenuItem
+														onClick={() =>
+															openDialog("createPermitEntry", {
+																date: selectedDate,
+															})
+														}
+													>
+														Permesso
+													</ContextMenuItem>
+													<ContextMenuItem
+														onClick={() =>
+															openDialog("createSickEntry", {
+																date: selectedDate,
+															})
+														}
+													>
+														Malattia
+													</ContextMenuItem>
+												</ContextMenuSubContent>
+											</ContextMenuSub>
+										) : null}
+									</ContextMenuContent>
+								</ContextMenu>
+							)
+						})}
+					</div>
+				</React.Fragment>
+			)}
+
+			{viewType === "week" && (
+				<div className="p-4 text-center text-muted-foreground text-sm">
+					🚧 Vista settimanale in sviluppo
 				</div>
-			</div>
-		</React.Fragment>
+			)}
+
+			{viewType === "day" && (
+				<div className="p-4 text-center text-muted-foreground text-sm">
+					🚧 Vista giornaliera in sviluppo
+				</div>
+			)}
+		</div>
 	)
 }
