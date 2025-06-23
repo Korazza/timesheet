087a2af81e80ebe87a82cef3619ac231f2ee8240
Roberto Coratti <corattiroberto@gmail.com>
2025-06-23 20:14:22 +0200

    changes

--- src/components/dialogs/employee-create-dialog.tsx
+++ src/components/dialogs/employee-create-dialog.tsx
@@ -3,29 +3,29 @@
 import { useTranslations } from "next-intl"
 
 import {
-  Dialog,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
+	Dialog,
+	DialogContent,
+	DialogHeader,
+	DialogTitle,
 } from "@/components/ui/dialog"
 import { EmployeeCreateForm } from "@/components/forms/employee-create-form"
 import { useDialog } from "@/hooks/use-dialog"
 
 export function EmployeeCreateDialog() {
-  const { activeDialog, closeDialog } = useDialog()
-  const t = useTranslations("Dialog.Employee")
+	const { activeDialog, closeDialog } = useDialog()
+	const t = useTranslations("Dialog.Employee")
 
-  return (
-    <Dialog
-      open={activeDialog === "createEmployee"}
-      onOpenChange={(open) => !open && closeDialog()}
-    >
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>{t("createTitle")}</DialogTitle>
-        </DialogHeader>
-        <EmployeeCreateForm />
-      </DialogContent>
-    </Dialog>
-  )
+	return (
+		<Dialog
+			open={activeDialog === "createEmployee"}
+			onOpenChange={(open) => !open && closeDialog()}
+		>
+			<DialogContent>
+				<DialogHeader>
+					<DialogTitle>{t("createTitle")}</DialogTitle>
+				</DialogHeader>
+				<EmployeeCreateForm />
+			</DialogContent>
+		</Dialog>
+	)
 }
